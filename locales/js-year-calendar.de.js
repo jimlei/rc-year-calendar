050a1e95daf57ce749c7e5b15c8e94c2d644fd2d
Jim Leirvik <jim@jimleirvik.no>
2023-10-31 09:48:56 +0100

    removed locales

--- locales/js-year-calendar.de.js
+++
@@ -1,9 +0,0 @@
-Calendar.locales['no'] = {
-	days: ["Søndag", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag"],
-	daysShort: ["Søn", "Man", "Tir", "Ons", "Tor", "Fre", "Lør"],
-	daysMin: ["Sø", "Ma", "Ti", "On", "To", "Fr", "Lø"],
-	months: ["Januar", "Februar", "Mars", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Desember"],
-	monthsShort: ["Jan", "Feb", "Mar", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Des"],
-	weekShort: 'U',
-	weekStart: 1
-};
